abdcdfbfa13e2169c944eb2930e9aa58e959d109
Pandu Prastiyo Aji <46983155+panduprastiyoaji@users.noreply.github.com>
2023-06-02 05:04:18 +0700

    Update NewsActivity.kt
    - Reduces code
    - Fix Bug

--- MD/app/src/main/java/com/example/projectcapstones/ui/news/NewsActivity.kt
+++ MD/app/src/main/java/com/example/projectcapstones/ui/news/NewsActivity.kt
@@ -20,17 +20,63 @@ import com.example.projectcapstones.repository.ResultNews
 
 class NewsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsBinding
+    private lateinit var newsAdapter: NewsAdapter
+    private val factory: ViewModelFactory = ViewModelFactory.getInstance()
+    private val viewModel: NewsViewModel by viewModels { factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getNews()
+        newsAdapter = NewsAdapter { news ->
+            val intent = Intent(this@NewsActivity, WebActivity::class.java)
+            intent.putExtra("url", news.url)
+            Toast.makeText(this, "Mohon tunggu sebentar", Toast.LENGTH_LONG).show()
+            startActivity(intent)
+        }
         setupView()
+        binding.rvList.layoutManager = LinearLayoutManager(this)
+        binding.rvList.adapter = newsAdapter
         binding.swipeRefreshLayout.setOnRefreshListener {
             binding.bannerSearch.searchBar.setText("")
-            getNews()
         }
+        binding.bannerSearch.searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { getNews() }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        getNews()
+    }
+
+    private fun getNews() {
+        viewModel.getHeadlineNews(binding.bannerSearch.searchBar.text.toString())
+            .observe(this@NewsActivity) { result ->
+                when (result) {
+                    is ResultNews.Loading -> {
+                        binding.progressBar.visibility = View.VISIBLE
+                    }
+                    is ResultNews.Success -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.rvList.visibility = View.VISIBLE
+                        val newsData = result.data
+                        newsAdapter.submitList(newsData)
+                        if (newsData.isEmpty()) {
+                            binding.viewError.root.visibility = View.VISIBLE
+                            binding.rvList.visibility = View.GONE
+                            binding.viewError.tvError.text = getString(R.string.notFound)
+                        } else {
+                            binding.viewError.root.visibility = View.GONE
+                        }
+                    }
+                    is ResultNews.Error -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.rvList.visibility = View.GONE
+                        binding.viewError.root.visibility = View.VISIBLE
+                        binding.viewError.tvError.text = getString(R.string.error)
+                    }
+                }
+                binding.swipeRefreshLayout.isRefreshing = false
+            }
     }
 
     private fun setupView() {
@@ -44,72 +90,5 @@ class NewsActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
-    }
-
-    private fun getNews() {
-        val factory: ViewModelFactory = ViewModelFactory.getInstance()
-        val viewModel: NewsViewModel by viewModels {
-            factory
-        }
-        val newsAdapter = NewsAdapter { news ->
-            val intent = Intent(this@NewsActivity, WebActivity::class.java)
-            intent.putExtra("url", news.url)
-            Toast.makeText(this, "Mohon tunggu sebentar", Toast.LENGTH_LONG).show()
-            startActivity(intent)
-        }
-
-        binding.rvList.layoutManager = LinearLayoutManager(this)
-        binding.rvList.adapter = newsAdapter
-        viewModel.getHeadlineNews().observe(this) { result ->
-            when (result) {
-                is ResultNews.Loading -> {
-                    binding.progressBar.visibility = View.VISIBLE
-                }
-                is ResultNews.Success -> {
-                    binding.progressBar.visibility = View.GONE
-                    val newsData = result.data
-                    newsAdapter.submitList(newsData)
-                }
-                is ResultNews.Error -> {
-                    binding.progressBar.visibility = View.GONE
-                    binding.viewError.root.visibility = View.VISIBLE
-                    binding.viewError.tvError.text = getString(R.string.error)
-                }
-            }
-            binding.swipeRefreshLayout.isRefreshing = false
-        }
-        binding.bannerSearch.searchBar.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.getHeadlineNews(s.toString()).observe(this@NewsActivity) { result ->
-                    when (result) {
-                        is ResultNews.Loading -> {
-                            binding.progressBar.visibility = View.VISIBLE
-                        }
-                        is ResultNews.Success -> {
-                            binding.progressBar.visibility = View.GONE
-                            binding.rvList.visibility = View.VISIBLE
-                            val newsData = result.data
-                            newsAdapter.submitList(newsData)
-                            if (newsData.isEmpty()) {
-                                binding.viewError.root.visibility = View.VISIBLE
-                                binding.rvList.visibility = View.GONE
-                                binding.viewError.tvError.text = getString(R.string.notFound)
-                            } else {
-                                binding.viewError.root.visibility = View.GONE
-                            }
-                        }
-                        is ResultNews.Error -> {
-                            binding.progressBar.visibility = View.GONE
-                            binding.rvList.visibility = View.GONE
-                            binding.viewError.root.visibility = View.VISIBLE
-                            binding.viewError.tvError.text = getString(R.string.error)
-                        }
-                    }
-                    binding.swipeRefreshLayout.isRefreshing = false
-                }
-            }
-            override fun afterTextChanged(s: Editable?) {}
-        })
     }
 }
