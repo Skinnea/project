a378479647919c1c6b5bca43e730c743369f8260
Pandu Prastiyo Aji <46983155+panduprastiyoaji@users.noreply.github.com>
2023-06-10 23:15:37 +0700

    Delete
    salah kirim file

--- MD/app/src/main/java/com/example/projectcapstones/data/ResultNews.kt
+++
@@ -1,7 +0,0 @@
-package com.example.projectcapstones.data
-
-sealed class ResultNews<out R> private constructor() {
-    data class Success<out T>(val data: T) : ResultNews<T>()
-    data class Error(val error: String) : ResultNews<Nothing>()
-    object Loading : ResultNews<Nothing>()
-}
